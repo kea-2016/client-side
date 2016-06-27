3b44ff9f1e163d3a36aa095590426464f5f97ee8
shakhoond <shayan.akhoond@gmail.com>
2016-06-28 10:59:14 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var greeting = require('./views/greeting.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
+var endpoint = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+console.log(endpoint)
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+xhr.get(endpoint, function (err, data) {
+  if (err) {
+    console.error(err)
+  }
+
+  // In case you're curious
+  console.log(data.body) // FYI: data.body is a string
+
+  // Replace 'Space' below with the response
+  var target = document.getElementsByTagName('main')[0]
+  target.innerHTML = greeting({name: 'Space'})
 })
-
